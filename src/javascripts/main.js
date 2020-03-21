7f64b628a4805451c73d8fdf0f0d57b3ef37a7e3
Anca Simon <ancarsimon@gmail.com>
2020-03-21 11:06:22 -0500

    added auth

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,15 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import authData from './helpers/data/authData';
+import auth from './components/auth/auth';
+
 import 'bootstrap';
 import './styles/main.scss';
 
-console.error('cows');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+};
+
+init();
