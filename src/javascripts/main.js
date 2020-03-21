a85f154d55aa0420767fb1a9297493238361ee8d
Anca Simon <ancarsimon@gmail.com>
2020-03-21 11:14:38 -0500

    additional auth

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 import authData from './helpers/data/authData';
+import myNavBar from './components/myNavBar/myNavBar';
 import auth from './components/auth/auth';
 
 import 'bootstrap';
@@ -10,6 +11,7 @@ const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
+  myNavBar.logoutEvent();
 };
 
 init();
