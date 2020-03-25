24400c616dbff545cc6a9f2194f773fa248cd9c6
Anca Simon <ancarsimon@gmail.com>
2020-03-24 20:03:10 -0500

    farmers and cows

--- src/javascripts/components/singleFarmer/singleFarmer.js
+++ src/javascripts/components/singleFarmer/singleFarmer.js
@@ -1,17 +1,22 @@
-import farmerData from '../../helpers/data/farmerData';
+import smash from '../../helpers/data/smash';
 import utils from '../../helpers/utils';
 
 const buildFarmer = (e) => {
   const farmerId = e.target.closest('.card').id;
-  farmerData.getFarmerById(farmerId)
-    .then((response) => {
-      const singleFarmer = response.data;
+  smash.getSingleFarmerWithCows(farmerId)
+    .then((singleFarmer) => {
       let domString = '';
       domString += '<h2 class="text-center">Featured Farmer</h2>';
       domString += '<div class="col-12">';
       domString += '<div class="card border-dark mb-3" style="max-width: 18rem;">';
       domString += `<div class="card-header">Farmer ${singleFarmer.name}</div>`;
-      domString += `<div class="card-body text-dark">Age: ${singleFarmer.age}</div>`;
+      domString += '<div class="card-body">';
+      domString += `<h6 class="text-dark">Age: ${singleFarmer.age}</h6>`;
+      domString += '<h3 class="card-title">Cow(s) Owned:</h3>';
+      singleFarmer.cows.forEach((cow) => {
+        domString += `<p class="card-text">${cow.name} (${cow.location})</p>`;
+      });
+      domString += '</div>';
       domString += '</div>';
       utils.printToDom('single-farmer', domString);
     })
@@ -19,6 +24,5 @@ const buildFarmer = (e) => {
 };
 
 // because this function gets executed when a click happens, then it gets an e as a param
-
 
 export default { buildFarmer };
---
+++ src/javascripts/helpers/data/farmerCowData.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getFarmerCowsByFarmerUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/farmerCows.json?orderBy="farmerUid"&equalTo="${uid}"`)
+  // logic above = orde rbyth ething in the collection that you want =whic is equal to the thign that you are passing in to this function
+    .then((response) => {
+      const demFarmerCows = response.data;
+      const farmerCows = [];
+      // this gets us a list of objects > next, we are turning into an array and then looping over them:
+      Object.keys(demFarmerCows).forEach((farmerCowId) => {
+        // now we select the object specified inbrackets and then addign an array of Id to it equal to the object which is the id in our case
+        demFarmerCows[farmerCowId].id = farmerCowId;
+        farmerCows.push(demFarmerCows[farmerCowId]);
+      });
+      resolve(farmerCows);
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getFarmerCowsByFarmerUid };
